afa4bcda6ccd031fe1adc28f24c123eb6861d272
Gilberto Silva <betosilva.luz@gmail.com>
2021-04-23 21:18:23 -0300

    Criando projeto.

---
+++ Snapchat Clone/Snapchat Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapchat Clone
+//
+//  Created by Gilberto Silva on 23/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
