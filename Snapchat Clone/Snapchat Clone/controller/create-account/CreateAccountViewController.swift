f51ebba8a3f0fbde07bc28027ee75eb995c0f3e7
Gilberto Silva <gilberto.silva@sigga.com>
2021-11-21 17:36:09 -0300

    Tratado exibição do teclado ao editar os campos de senha na tela de criar conta.

--- Snapchat Clone/Snapchat Clone/controller/create-account/CreateAccountViewController.swift
+++ Snapchat Clone/Snapchat Clone/controller/create-account/CreateAccountViewController.swift
@@ -29,6 +29,9 @@ class CreateAccountViewController: HomeNavigationBaseViewController {
         configureFilledTextField(textField: confirmPasswordTextField)
         configureFilledTextField(textField: fullNameTextField)
         configureBind()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     private func configureBind(){
@@ -56,4 +59,24 @@ class CreateAccountViewController: HomeNavigationBaseViewController {
         viewModel.createAccount()
     }
     
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        
+        if passwordTextField.isEditing || confirmPasswordTextField.isEditing {
+            if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+                if self.view.frame.origin.y == 0 {
+                    self.view.frame.origin.y -= keyboardSize.height
+                }
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
 }
