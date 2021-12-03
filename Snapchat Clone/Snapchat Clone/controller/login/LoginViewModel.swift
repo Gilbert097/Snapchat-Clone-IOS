add9e7743286a46f7ab88d7d5c9696aef71da788
Gilberto Silva <betosilva.luz@gmail.com>
2021-12-02 22:16:05 -0300

    Recuperando usuário logado.

--- Snapchat Clone/Snapchat Clone/controller/login/LoginViewModel.swift
+++ Snapchat Clone/Snapchat Clone/controller/login/LoginViewModel.swift
@@ -8,14 +8,20 @@
 import Foundation
 
 public class LoginViewModel: LoginViewModelProtocol {
+    private static let TAG = "LoginViewModel"
     
     private var email: String = ""
     private var password: String = ""
     private let output = Event<EventData<LoginEventType>>(.init(type: .none))
     private let authenticationService: UserAuthenticationServiceProtocol
+    private let userRepository: UserRepositoryProtocol
     
-    init(authenticationService: UserAuthenticationServiceProtocol) {
+    init(
+        authenticationService: UserAuthenticationServiceProtocol,
+        userRepository: UserRepositoryProtocol
+    ) {
         self.authenticationService = authenticationService
+        self.userRepository = userRepository
     }
     
     func bind(input: Input) -> Output {
@@ -28,10 +34,15 @@ public class LoginViewModel: LoginViewModelProtocol {
         if !email.isEmpty && !password.isEmpty {
             self.authenticationService.signIn(email: email, password: password) { [weak self] (userId, error) in
                 guard let self = self else { return }
-                if userId != nil {
-                    //TODO[GIL] - Recuperar usuário do banco e setar no AppRepository
-                    self.output.value = .init(type: .navigationToMain, info: nil)
+                if let userId = userId {
+                    LogUtils.printMessage(tag: LoginViewModel.TAG, message: "UserId -> \(userId)")
+                    self.userRepository.registerObserveUser(id: userId) { user in
+                        LogUtils.printMessage(tag: LoginViewModel.TAG, message: "Set current user \(String(describing: user?.fullName))")
+                        AppRepository.shared.setCurrentUser(currentUser: user)
+                        self.output.value = .init(type: .navigationToMain, info: nil)
+                    }
                 } else if let error = error {
+                    LogUtils.printMessage(tag: LoginViewModel.TAG, message: "Error -> \(error)")
                     self.output.value = .init(type: .showMessage, info: InfoAlertViewModel(title: "Error", message: error))
                 }
             }
