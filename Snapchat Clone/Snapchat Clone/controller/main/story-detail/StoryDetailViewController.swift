6f4e2dc0b7e04aecd3613091a4ad3593e7339f26
Gilberto Silva <betosilva.luz@gmail.com>
2021-12-23 15:56:34 -0300

    Refatorando código.

--- Snapchat Clone/Snapchat Clone/controller/main/story-detail/StoryDetailViewController.swift
+++ Snapchat Clone/Snapchat Clone/controller/main/story-detail/StoryDetailViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 
 class StoryDetailViewController: UIViewController {
-    private let maxStorys = 30
-    private var storysCount: Int = 0
+    private static let TAG = "StoryDetailViewController"
+    
     public var story:StoryItemViewModel! {
         didSet {
             storysCount  = story.count < maxStorys ? story.count : maxStorys
@@ -18,32 +18,19 @@ class StoryDetailViewController: UIViewController {
     }
     public let progressIndicatorViewTag = 88
     public let progressViewTag = 99
+    private let maxStorys = 30
+    private var storysCount: Int = 0
+    private var progressView: UIView!
     
-    
-    private var progressView: UIView?
-    public var getProgressView: UIView {
-        if let progressView = self.progressView {
-            return progressView
-        }
-        let v = UIView()
-        v.translatesAutoresizingMaskIntoConstraints = false
-        self.progressView = v
-        self.view.addSubview(self.getProgressView)
-        return v
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let pv = getProgressView
-        NSLayoutConstraint.activate([
-            pv.leftAnchor.constraint(equalTo: self.view.leftAnchor),
-            pv.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8),
-            self.view.rightAnchor.constraint(equalTo: pv.rightAnchor),
-            pv.heightAnchor.constraint(equalToConstant: 10)
-        ])
-        
-        //-------------------------------------------------------------------------
-        print("Progressor count: \(getProgressView.subviews.count)")
+        self.progressView = createProgressBarView()
+        configLayoutConstraints()
+        loadStorysBarView()
+    }
+    
+    private func loadStorysBarView() {
+        LogUtils.printMessage(tag: StoryDetailViewController.TAG, message: "Progressor count: \(progressView.subviews.count)")
         let padding: CGFloat = 8 //GUI-Padding
         let height: CGFloat = 3
         var storyBarViewArray: [StoryBarView] = []
@@ -51,46 +38,63 @@ class StoryDetailViewController: UIViewController {
         
         for index in 0..<storysCount{
             let storyBarView = createStoryBarView(tag: index+progressIndicatorViewTag)
-            getProgressView.addSubview(storyBarView)
+            progressView.addSubview(storyBarView)
             storyBarViewArray.append(storyBarView)
             
-//            let pv = IGSnapProgressView()
-//            pv.translatesAutoresizingMaskIntoConstraints = false
-//            pvIndicator.addSubview(applyProperties(pv))
-//            pvArray.append(pv)
+            //            let pv = IGSnapProgressView()
+            //            pv.translatesAutoresizingMaskIntoConstraints = false
+            //            pvIndicator.addSubview(applyProperties(pv))
+            //            pvArray.append(pv)
         }
         
-        // Setting Constraints for all progressView indicators
+        // Setting Constraints for all storys bar
         for index in 0..<storyBarViewArray.count {
-            let pvIndicator = storyBarViewArray[index]
+            let storyBarCurrent = storyBarViewArray[index]
             if index == 0 {
-                pvIndicator.leftConstraiant = pvIndicator.leftAnchor.constraint(equalTo: self.getProgressView.leftAnchor, constant: padding)
+                storyBarCurrent.leftConstraiant = storyBarCurrent.leftAnchor.constraint(equalTo: self.progressView.leftAnchor, constant: padding)
                 NSLayoutConstraint.activate([
-                    pvIndicator.leftConstraiant!,
-                    pvIndicator.centerYAnchor.constraint(equalTo: self.getProgressView.centerYAnchor),
-                    pvIndicator.heightAnchor.constraint(equalToConstant: height)
-                    ])
+                    storyBarCurrent.leftConstraiant!,
+                    storyBarCurrent.centerYAnchor.constraint(equalTo: self.progressView.centerYAnchor),
+                    storyBarCurrent.heightAnchor.constraint(equalToConstant: height)
+                ])
                 if storyBarViewArray.count == 1 {
-                    pvIndicator.rightConstraiant = self.getProgressView.rightAnchor.constraint(equalTo: pvIndicator.rightAnchor, constant: padding)
-                    pvIndicator.rightConstraiant!.isActive = true
+                    storyBarCurrent.rightConstraiant = self.progressView.rightAnchor.constraint(equalTo: storyBarCurrent.rightAnchor, constant: padding)
+                    storyBarCurrent.rightConstraiant!.isActive = true
                 }
             }else {
-                let prePVIndicator = storyBarViewArray[index-1]
-                pvIndicator.widthConstraint = pvIndicator.widthAnchor.constraint(equalTo: prePVIndicator.widthAnchor, multiplier: 1.0)
-                pvIndicator.leftConstraiant = pvIndicator.leftAnchor.constraint(equalTo: prePVIndicator.rightAnchor, constant: padding)
+                let storyBarPrevious = storyBarViewArray[index-1]
+                storyBarCurrent.widthConstraint = storyBarCurrent.widthAnchor.constraint(equalTo: storyBarPrevious.widthAnchor, multiplier: 1.0)
+                storyBarCurrent.leftConstraiant = storyBarCurrent.leftAnchor.constraint(equalTo: storyBarPrevious.rightAnchor, constant: padding)
                 NSLayoutConstraint.activate([
-                    pvIndicator.leftConstraiant!,
-                    pvIndicator.centerYAnchor.constraint(equalTo: prePVIndicator.centerYAnchor),
-                    pvIndicator.heightAnchor.constraint(equalToConstant: height),
-                    pvIndicator.widthConstraint!
-                    ])
-                if index == storyBarViewArray.count-1 {
-                    pvIndicator.rightConstraiant = self.view.rightAnchor.constraint(equalTo: pvIndicator.rightAnchor, constant: padding)
-                    pvIndicator.rightConstraiant!.isActive = true
+                    storyBarCurrent.leftConstraiant!,
+                    storyBarCurrent.centerYAnchor.constraint(equalTo: storyBarPrevious.centerYAnchor),
+                    storyBarCurrent.heightAnchor.constraint(equalToConstant: height),
+                    storyBarCurrent.widthConstraint!
+                ])
+                let isStoryBarLast = index == (storyBarViewArray.count - 1)
+                if  isStoryBarLast {
+                    storyBarCurrent.rightConstraiant = self.view.rightAnchor.constraint(equalTo: storyBarCurrent.rightAnchor, constant: padding)
+                    storyBarCurrent.rightConstraiant!.isActive = true
                 }
             }
         }
-        
+    }
+    
+    
+    private func configLayoutConstraints() {
+        NSLayoutConstraint.activate([
+            progressView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            progressView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 8),
+            self.view.rightAnchor.constraint(equalTo: progressView.rightAnchor),
+            progressView.heightAnchor.constraint(equalToConstant: 10)
+        ])
+    }
+    
+    private func createProgressBarView() -> UIView {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(v)
+        return v
     }
     
     private func createStoryBarView(tag: Int? = nil) -> StoryBarView {
