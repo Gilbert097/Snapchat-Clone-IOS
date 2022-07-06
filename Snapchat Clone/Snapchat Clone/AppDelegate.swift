d84d11a94812b3644182711834b3e4e6feac6701
Gilberto Silva <betosilva.luz@gmail.com>
2022-07-06 20:08:20 -0300

    add FirebaseAppMessaging.

--- Snapchat Clone/Snapchat Clone/AppDelegate.swift
+++ Snapchat Clone/Snapchat Clone/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import FirebaseInstallations
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        FirebaseApp.initialize()
+        Installations.installations().installationID { identifier, error in
+          guard let installationID = identifier else {
+            print("Error getting installation ID: \(error)")
+              return
+          }
+          
+          print("Firebase installations ID = \(installationID)")
+        }
         return true
     }
     
